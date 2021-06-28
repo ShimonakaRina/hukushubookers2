94446093381e83ac3f2878637e747127bcf5a371
ShimonakaRina <sixseasonrain11614@gmail.com>
2021-06-28 12:24:03 +0000

    application.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-
+get 'home/about' => 'homes#about'
 root to: 'homes#top'
-  devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :edit, :update]
+devise_for :users
+resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+resources :users, only: [:index, :show, :edit, :update]
 end
